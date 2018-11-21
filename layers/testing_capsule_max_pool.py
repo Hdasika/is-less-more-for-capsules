bf500c7efd5d88610ecf590879647f7ee10ef67e
Btara Truhandarien <btruhand@sfu.ca>
2018-11-20 16:32:22 -0800

    Updated capsule max pooling test to be more precise

--- layers/testing_capsule_max_pool.py
+++ layers/testing_capsule_max_pool.py
@@ -1,20 +1,54 @@
 from capsule_max_pool import CapsMaxPool
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input
-from tensorflow.keras.optimizers import Adam
+from tensorflow.train import AdamOptimizer
 import numpy as np
 import tensorflow as tf
 
-# tf.enable_eager_execution()
+tf.enable_eager_execution()
 
-shape = (1,4,4,7,8)
+shape = (1,2,2,2,3)
 x = Input(shape=shape[1:])
 maxpool = CapsMaxPool()(x)
 model = Model(inputs=x, outputs=maxpool)
-input_x = np.random.rand(*shape) * 100
-input_y = np.random.rand(*(1,2,2,7,8)) * 100
+input_x = np.array([
+	[
+		[
+			[
+				[1,2,3],
+				[1,2,3]
+			],
+			[
+				[4,5,6],
+				[4,5,6]
+			]
+		],
+		[
+			[
+				[7,8,9],
+				[7,8,9]
+			],
+			[
+				[10,11,12],
+				[10,11,12]
+			]
+		],
+	]
+], dtype=np.float32)
+input_y = np.array([
+	[
+		[
+			[
+				[10,11,12],
+				[10,11,12]
+			],
+		]
+	]
+], dtype=np.float32)
 tensor_x = tf.cast(input_x, dtype=tf.float32)
 tensor_y = tf.cast(input_y, dtype=tf.float32)
-opt = Adam()
+opt = AdamOptimizer()
 model.compile(optimizer=opt, loss='mean_squared_error')
 print(model.fit(x=input_x, y=input_y, batch_size=1))
+
+model.compile(optimizer=opt, loss='mean_squared_error')
